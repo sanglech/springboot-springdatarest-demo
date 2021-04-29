b14add8e06fb5664c8cc67e96b9437eb5b8303b5
Christian <christian@sanglech.com>
2021-04-29 14:26:49 +0900

    Changed properties for JPA Springdata

--- src/main/java/com/christian/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/christian/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.christian.cruddemo.dao;
 
 import com.christian.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
     //finished.... all crud methods come from SpringDataJPA
 }
